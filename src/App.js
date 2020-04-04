921fbfc1c5fe53fb5d22b7a0e19a2a90249ee6a1
mohan-6543 <mohanraj.kc@zohocorp.com>
2020-04-04 14:11:47 +0530

    Test

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component
             <div className="profile-container">
                 <img className="profile" src="assets/MJ.jpeg" alt="" width="180" height="180"/>
                 <h1 className="name">{resume.name}</h1>
-                <h3 className="tagline">Developer</h3>
+                <h3 className="tagline">Full Stack Developer</h3>
             </div>
             
             <div className="contact-container container-block">
